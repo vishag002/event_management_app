4af232aab6d7ded2c6b78957b1e459aec3fc9fc6
vishag002 <vvishag477@gmail.com>
2024-11-21 14:57:47 +0530

    modified

--- lib/views/vendor/vendor_add_service_screen.dart
+++ lib/views/vendor/vendor_add_service_screen.dart
@@ -1,290 +1,232 @@
-import 'package:animated_custom_dropdown/custom_dropdown.dart';
 import 'package:event_management_app/utilis/text_const.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:animated_custom_dropdown/custom_dropdown.dart';
 
-class VendorAddServiceScreen extends StatelessWidget {
-  const VendorAddServiceScreen({super.key});
+class VendorAddServiceScreen extends StatefulWidget {
+  const VendorAddServiceScreen({Key? key}) : super(key: key);
 
   @override
+  _VendorAddServiceScreenState createState() => _VendorAddServiceScreenState();
+}
+
+class _VendorAddServiceScreenState extends State<VendorAddServiceScreen> {
+  @override
   Widget build(BuildContext context) {
-    final TextEditingController titleController = TextEditingController();
-    final TextEditingController descriptionController = TextEditingController();
-    final TextEditingController priceController = TextEditingController();
+    final TextEditingController _titleController = TextEditingController();
+    final TextEditingController _descriptionController =
+        TextEditingController();
+    final TextEditingController _priceController = TextEditingController();
+
+    String _selectedPriceType = priceType[0];
+    String _selectedCategory = categoryList[0];
     return Scaffold(
       appBar: AppBar(
+        elevation: 0,
         leading: IconButton(
-            onPressed: () {
-              Navigator.pop(context);
-            },
-            icon: Icon(Icons.arrow_back)),
-        title: Text("Add Service"),
+          icon: Icon(Icons.arrow_back, color: Colors.black),
+          onPressed: () => Navigator.pop(context),
+        ),
+        title: Text("Add Service", style: TextConstants.appTitle),
         actions: [
           IconButton(
-            onPressed: () {
-              Navigator.pop(context);
-            },
-            icon: Icon(
-              CupertinoIcons.checkmark_alt,
-              size: 30,
-            ),
-          )
+            icon: Icon(CupertinoIcons.checkmark_alt,
+                color: Colors.black, size: 30),
+            onPressed: _saveService,
+          ),
         ],
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: ListView(
-          children: [
-            //add title
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20),
-              child: Text(
-                "Service Title",
-                style: TextConstants.subheading,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              _buildSectionTitle("Service Title"),
+              _buildCustomTextField(
+                controller: _titleController,
+                hintText: 'Enter service title',
+                validator: _validateNotEmpty,
               ),
-            ),
-            ListTile(
-              title: CustomTextField(
-                controller: titleController,
-                hintText: 'Add title',
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter some text';
-                  }
-                  return null;
+              SizedBox(height: 16),
+              _buildSectionTitle("Description"),
+              _buildCustomTextField(
+                controller: _descriptionController,
+                hintText: 'Describe your service',
+                maxLines: 3,
+                validator: _validateNotEmpty,
+              ),
+              SizedBox(height: 16),
+              _buildSectionTitle("Price"),
+              _buildCustomTextField(
+                controller: _priceController,
+                hintText: 'Enter service price',
+                keyboardType: TextInputType.number,
+                validator: _validateNotEmpty,
+              ),
+              SizedBox(height: 16),
+              _buildSectionTitle("Price Type"),
+              _buildCustomDropdown(
+                value: _selectedPriceType,
+                items: priceType,
+                onChanged: (value) {
+                  setState(() {
+                    _selectedPriceType = value!;
+                  });
                 },
               ),
-            ),
-
-            //add description
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20),
-              child: Text(
-                "Discription",
-                style: TextConstants.subheading,
-              ),
-            ),
-            ListTile(
-              title: CustomTextField(
-                controller: descriptionController,
-                hintText: 'Add description',
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter some text';
-                  }
-                  return null;
+              SizedBox(height: 16),
+              _buildSectionTitle("Category"),
+              _buildCustomDropdown(
+                value: _selectedCategory,
+                items: categoryList,
+                onChanged: (value) {
+                  setState(() {
+                    _selectedCategory = value!;
+                  });
                 },
               ),
-            ),
-
-            //add price
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20),
-              child: Text(
-                "Price",
-                style: TextConstants.subheading,
-              ),
-            ),
-            ListTile(
-              title: CustomTextField(
-                controller: priceController,
-                hintText: 'Add Service price',
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter some text';
-                  }
-                  return null;
-                },
-              ),
-            ),
-
-            //select price Type
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20),
-              child: Text(
-                "Select Price Type",
-                style: TextConstants.subheading,
-              ),
-            ),
-            ListTile(
-              title: Container(
-                height: 60,
-                width: double.infinity,
-                decoration: BoxDecoration(
-                  border: Border.all(
-                    color: Colors.black,
-                    width: 1,
-                  ),
-                  borderRadius: BorderRadius.circular(8),
-                ),
-                child: CustomDropdown<String>(
-                  hintText: 'Select price type',
-                  items: priceType,
-                  initialItem: priceType[0],
-                  onChanged: (value) {
-                    print('changing value to: $value');
-                  },
-                ),
-              ),
-            ),
-
-            //select category
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20),
-              child: Text(
-                "Category",
-                style: TextConstants.subheading,
-              ),
-            ),
-            ListTile(
-              title: Container(
-                height: 60,
-                width: double.infinity,
-                decoration: BoxDecoration(
-                  border: Border.all(
-                    color: Colors.black,
-                    width: 1,
-                  ),
-                  borderRadius: BorderRadius.circular(8),
-                ),
-                child: CustomDropdown<String>(
-                  hintText: 'Select Category',
-                  items: categoryList,
-                  initialItem: categoryList[0],
-                  onChanged: (value) {
-                    print('changing value to: $value');
-                  },
-                ),
-              ),
-            ),
-
-            //select image
-            SizedBox(height: 30),
-            ListTile(
-              title: Container(
-                width: double.infinity,
-                height: 55,
-                decoration: BoxDecoration(
-                  color: Colors.black,
-                  borderRadius: BorderRadius.circular(8),
-                ),
-                child: const Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Icon(
-                      Icons.add,
-                      color: Colors.white,
-                      size: 25,
-                    ),
-                    Text(
-                      "Add Image",
-                      style: TextConstants.buttonText,
-                    ),
-                  ],
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: GridView.builder(
-                shrinkWrap: true,
-                //scrollDirection: Axis.vertical,
-                physics: NeverScrollableScrollPhysics(),
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2,
-                  mainAxisSpacing: 10,
-                  crossAxisSpacing: 10,
-                  childAspectRatio: 1 / 1.5,
-                ),
-                itemCount: 4,
-                itemBuilder: (context, index) {
-                  return Container(
-                    decoration: BoxDecoration(
-                      color: Colors.grey,
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    alignment: Alignment.center,
-                    // child: Text(
-                    //   'Item $index',
-                    //   style: const TextStyle(color: Colors.white),
-                    // ),
-                  );
-                },
-              ),
-            ),
-          ],
+              SizedBox(height: 24),
+              _buildImageUploadSection(),
+            ],
+          ),
         ),
       ),
     );
   }
-}
 
-////
-class CustomTextField extends StatelessWidget {
-  final TextEditingController? controller;
-  final String? hintText;
-  final String? labelText;
-  final bool obscureText;
-  final TextInputType? keyboardType;
-  final FormFieldValidator<String>? validator;
-  final bool enabled;
-  final int? maxLines;
-  final void Function(String)? onChanged;
+  Widget _buildSectionTitle(String title) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 8),
+      child: Text(
+        title,
+        style: TextConstants.subheading,
+      ),
+    );
+  }
 
-  const CustomTextField({
-    Key? key,
-    this.controller,
-    this.hintText,
-    this.labelText,
-    this.obscureText = false,
-    this.keyboardType,
-    this.validator,
-    this.enabled = true,
-    this.maxLines = 1,
-    this.onChanged,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
+  Widget _buildCustomTextField({
+    required TextEditingController controller,
+    required String hintText,
+    required String? Function(String?) validator,
+    int maxLines = 1,
+    TextInputType keyboardType = TextInputType.text,
+  }) {
     return TextFormField(
       controller: controller,
-      decoration: InputDecoration(
-        hintText: hintText,
-        labelText: labelText,
-        // Black and white theme
-        hintStyle: const TextStyle(color: Colors.grey),
-        labelStyle: const TextStyle(color: Colors.black),
-        enabledBorder: const OutlineInputBorder(
-          borderRadius: BorderRadius.all(Radius.circular(8)),
-          borderSide: BorderSide(color: Colors.black, width: 1.0),
-        ),
-        focusedBorder: const OutlineInputBorder(
-          borderRadius: BorderRadius.all(Radius.circular(8)),
-          borderSide: BorderSide(color: Colors.black, width: 1.5),
-        ),
-        disabledBorder: const OutlineInputBorder(
-          borderSide: BorderSide(color: Colors.grey, width: 1.0),
-        ),
-        errorBorder: const OutlineInputBorder(
-          borderSide: BorderSide(color: Colors.red, width: 1.0),
-        ),
-        focusedErrorBorder: const OutlineInputBorder(
-          borderSide: BorderSide(color: Colors.red, width: 1.5),
-        ),
-      ),
-      obscureText: obscureText,
+      maxLines: maxLines,
       keyboardType: keyboardType,
       validator: validator,
-      enabled: enabled,
-      maxLines: maxLines,
-      onChanged: onChanged,
-      style: TextConstants.bodyText,
-      cursorColor: Colors.black,
+      style: TextStyle(color: Colors.black),
+      decoration: InputDecoration(
+        hintText: hintText,
+        hintStyle: TextConstants.bodyTextSecondary,
+        filled: true,
+        fillColor: Colors.grey.shade100,
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(12),
+          borderSide: BorderSide.none,
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(12),
+          borderSide: BorderSide(color: Colors.black, width: 1.5),
+        ),
+      ),
     );
   }
-}
-///////drop down
 
+  Widget _buildCustomDropdown({
+    required String value,
+    required List<String> items,
+    required void Function(String?) onChanged,
+  }) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.grey.shade100,
+        borderRadius: BorderRadius.circular(12),
+      ),
+      child: CustomDropdown<String>(
+        hintText: 'Select',
+        items: items,
+        initialItem: value,
+        decoration: CustomDropdownDecoration(
+          hintStyle: TextConstants.bodyTextSecondary,
+          headerStyle: TextConstants.formLabel,
+          closedBorder: Border.all(color: Colors.transparent),
+          closedFillColor: Colors.transparent,
+          expandedBorder: Border.all(color: Colors.black, width: 1.5),
+        ),
+        onChanged: onChanged,
+      ),
+    );
+  }
+
+  Widget _buildImageUploadSection() {
+    return Column(
+      children: [
+        Container(
+          width: double.infinity,
+          decoration: BoxDecoration(
+            color: Colors.black,
+            borderRadius: BorderRadius.circular(12),
+          ),
+          padding: EdgeInsets.symmetric(vertical: 16),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(Icons.add_photo_alternate, color: Colors.white, size: 28),
+              SizedBox(width: 10),
+              Text(
+                "Add Images",
+                style: TextConstants.buttonText,
+              ),
+            ],
+          ),
+        ),
+        SizedBox(height: 16),
+        GridView.builder(
+          shrinkWrap: true,
+          physics: NeverScrollableScrollPhysics(),
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            mainAxisSpacing: 12,
+            crossAxisSpacing: 12,
+            childAspectRatio: 1 / 1.2,
+          ),
+          itemCount: 4,
+          itemBuilder: (context, index) {
+            return Container(
+              decoration: BoxDecoration(
+                color: Colors.grey.shade200,
+                borderRadius: BorderRadius.circular(12),
+                border: Border.all(color: Colors.grey.shade300),
+              ),
+              child: Center(
+                child: Icon(
+                  Icons.image_outlined,
+                  color: Colors.grey.shade500,
+                  size: 50,
+                ),
+              ),
+            );
+          },
+        ),
+      ],
+    );
+  }
+
+  String? _validateNotEmpty(String? value) {
+    return value == null || value.isEmpty ? 'This field cannot be empty' : null;
+  }
+
+  void _saveService() {
+    //todo:- Implement save service logic
+    print('Service saved');
+    Navigator.pop(context);
+  }
+}
+
+// Existing lists remain the same
 const List<String> categoryList = [
   'Wedding',
   'Corporate',
